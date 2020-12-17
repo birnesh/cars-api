35864e5b940e90a7adc45508c52d567fd05a459c
Birnesh Eswar <birnesh711@gmail.com>
2020-12-18 00:16:50 +0900

    updated the model , schema and added retrieve API END Points

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from marshmallow import ValidationError, pre_load
+from sqlalchemy.exc import IntegrityError
 import os
 
 # Init app
 app = Flask(__name__)
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-# Database
+# configuration
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
     BASE_DIR, "db.sqlite"
 )
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["JSON_SORT_KEYS"] = False
 
 # Init db
 db = SQLAlchemy(app)
@@ -21,11 +24,14 @@ ma = Marshmallow(app)
 
 # DB Models
 class Manufacturer(db.Model):
+    __tablename__ = "manufacturer"
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True, nullable=False)
     head_quarter = db.Column(db.String(20), nullable=True)
     founder = db.Column(db.String(50), nullable=True)
     established_year = db.Column(db.Integer)
+    cars = db.relationship("Car", backref="manufacturer", lazy=True)
 
     def __init__(self, name, head_quarter, founder, established_year):
         self.name = name
@@ -34,27 +40,96 @@ class Manufacturer(db.Model):
         self.established_year = established_year
 
     def __repr__(self):
-        return f"<Manufacturer {self.name}"
+        return f"<Manufacturer {self.name}>"
+
+
+class Car(db.Model):
+    __tablename__ = "car"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    manufacturer_id = db.Column(
+        db.Integer, db.ForeignKey("manufacturer.id"), nullable=False
+    )
+    launched_year = db.Column(db.Integer)
+    top_speed = db.Column(db.Integer)
+    engine_type = db.Column(db.String(30), nullable=False)
+    max_horse_power = db.Column(db.Integer)
+    zero_to_hundred = db.Column(db.Float, nullable=True)
+
+    def __init__(
+        self,
+        name,
+        manufacturer_id,
+        launched_year,
+        top_speed,
+        engine_type,
+        max_horse_power,
+        zero_to_hundred,
+    ):
+        self.name = name
+        self.manufacturer_id = manufacturer_id
+        self.launched_year = launched_year
+        self.top_speed = top_speed
+        self.engine_type = engine_type
+        self.max_horse_power = max_horse_power
+        self.zero_to_hundred = zero_to_hundred
+
+    def __repr__(self):
+        return f"<Car {self.name}"
 
 
 # Schemas
-class ManufacturerSchema(ma.Schema):
+
+
+class ManufacturerSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
-        fields = ["name", "head_quarter", "founder", "established_year"]
+        ordered = True
+        model = Manufacturer
+
+
+class CarSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        ordered = True
+        fields = (
+            "id",
+            "name",
+            "launched_year",
+            "top_speed",
+            "engine_type",
+            "max_horse_power",
+            "zero_to_hundred",
+            "manufacturer",
+        )
+
+    manufacturer = ma.Nested("ManufacturerSchema")
 
 
 # Init schema
 manufacturer_schema = ManufacturerSchema()
 manufacturers_schema = ManufacturerSchema(many=True)
 
+car_schema = CarSchema()
+cars_schema = CarSchema(many=True)
 
+
+# API routes
 @app.route("/manufacturer", methods=["GET", "POST"])
-def add_manufacturer():
+def create_view_manufacturer():
     if request.method == "POST":
-        name = request.json["name"]
-        head_quarter = request.json["head_quarter"]
-        founder = request.json["founder"]
-        established_year = request.json["established_year"]
+        json_data = request.get_json()
+        if not json_data:
+            return {"message": "No input data provided"}, 400
+        # Validate and deserialize input
+        try:
+            data = manufacturer_schema.load(json_data)
+        except ValidationError as err:
+            return err.messages, 422
+        name = data["name"]
+        head_quarter = data["head_quarter"]
+        founder = data["founder"]
+        established_year = data["established_year"]
+
         try:
             new_manufacturer = Manufacturer(
                 name, head_quarter, founder, established_year
@@ -66,9 +141,73 @@ def add_manufacturer():
             return jsonify({"msg": E})
     else:
         all_manufacturers = Manufacturer.query.all()
-        result = manufacturers_schema.dump(all_manufacturers)
+        response = manufacturers_schema.dump(all_manufacturers)
 
-        return jsonify(result)
+        return jsonify(response)
+
+
+@app.route("/manufacturer/<int:id>", methods=["GET"])
+def retrieve_manufacturer(id):
+    try:
+        manufacturer = Manufacturer.query.filter_by(id=id).first()
+        assert manufacturer is not None
+        response = manufacturer_schema.dump(manufacturer)
+        return response
+    except AssertionError:
+        return jsonify({"msg": "record not found"})
+
+
+@app.route("/car", methods=["GET", "POST"])
+def create_view_car():
+    if request.method == "POST":
+        json_data = request.get_json()
+        if not json_data:
+            return {"message": "No input data provided"}, 400
+        # Validate and deserialize input
+        try:
+            data = car_schema.load(json_data)
+        except ValidationError as err:
+            return err.messages, 422
+
+        name = data["name"]
+        manufacturer_id = data["manufacturer_id"]
+        launched_year = data["launched_year"]
+        top_speed = data["top_speed"]
+        engine_type = data["engine_type"]
+        max_horse_power = data["max_horse_power"]
+        zero_to_hundred = data["zero_to_hundred"]
+
+        try:
+            new_car = Car(
+                name,
+                manufacturer_id,
+                launched_year,
+                top_speed,
+                engine_type,
+                max_horse_power,
+                zero_to_hundred,
+            )
+            db.session.add(new_car)
+            db.session.commit()
+            return car_schema.jsonify(new_car)
+        except Exception as E:
+            return jsonify({"msg": E})
+    else:
+        all_cars = Car.query.all()
+        response = cars_schema.dump(all_cars)
+
+        return jsonify(response)
+
+
+@app.route("/car/<int:id>", methods=["GET"])
+def retrieve_car(id):
+    try:
+        car = Car.query.filter_by(id=id).first()
+        assert car is not None
+        response = car_schema.dump(car)
+        return response
+    except AssertionError:
+        return jsonify({"msg": "record not found"})
 
 
 # Run server
