8621236761d4e1c7706ad18329a0b9c68e644df9
Birnesh Eswar <birnesh711@gmail.com>
2020-12-22 12:28:49 +0900

    added Flask-Migrate and requirements.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 from flask_marshmallow import Marshmallow
+from flask_migrate import Migrate
 
 
 # Define the WSGI application object
@@ -20,6 +21,7 @@ db = SQLAlchemy(app)
 # Init ma
 ma = Marshmallow(app)
 
+migrate = Migrate(app, db)
 # Import a module / component using its blueprint handler variable (mod_auth)
 from app.manfacturer_car_module.views import manufacturer_car_module
 
@@ -30,4 +32,3 @@ app.register_blueprint(manufacturer_car_module)
 
 # Build the database:
 # This will create the database file using SQLAlchemy
-db.create_all()
